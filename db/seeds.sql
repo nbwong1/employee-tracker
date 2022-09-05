5a551e04e9b09dc0cf2f619009d4c23a494d47a4
nbwong <nicholas.b.wong@gmail.com>
2022-09-05 15:13:48 -0400

    added more code for the tables and the seed data, need to update index.js so it can be run

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO departments(id, department_id)
+VALUES ();
+
+INSERT INTO roles('job title', 'role id', 'department', 'salary')
+VALUES ();
+
+INSERT INTO employees('employee_id', 'first_name', 'last_name', 'job_title', 'department', 'salaries', 'managers')
+VALUES ();
+       
